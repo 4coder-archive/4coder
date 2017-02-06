b1a416609a4c9f9a56a0c30752684c61ed4e3792
Allen Webster <editor@4coder.net>
2017-02-06 10:26:11 -0500

    buildsuper fix on linux

--- 4ed.cpp
+++ 4ed.cpp
@@ -7,6 +7,7 @@
  *
  */
 
+
 // TOP
 
 // App Structs
--- buildsuper.sh
+++ buildsuper.sh
@@ -11,7 +11,7 @@ if [ -z "$SOURCE" ]; then
     SOURCE="$CODE_HOME/4coder_default_bindings.cpp"
 fi
 
-echo "Building 4coder_custom.so from $SOURCE ..."
+echo "Building custom_4coders.so from $SOURCE ..."
 
-g++ -I"$CODE_HOME" -Wno-write-strings -std=gnu++0x "$SOURCE" -shared -o 4coder_custom.so -fPIC
+g++ -I"$CODE_HOME" -Wno-write-strings -std=gnu++0x "$SOURCE" -shared -o custom_4coder.so -fPIC
 
