27442c4322ac95818b61cbf3e7dc24c8854c3a38
Allen Webster <editor@4coder.net>
2017-02-27 09:15:36 -0500

    fixed issue with doc comment

--- 4coder_API/types.h
+++ 4coder_API/types.h
@@ -615,7 +615,7 @@ DOC_SEE(buffer_add_markers)
 STRUCT Marker{
     /* DOC(The current position of the marker measure in absolute byte positioning coordinates.) */
     int32_t pos;
-    /* DOC(When a marker is inside a range that gets edited, by default the marker 'leans_left' which means it goes to the beginning of the edited range.  If the field lean_right is set to true, the marker will lean right with edits and will go to the end of edited range.*/
+    /* DOC(When a marker is inside a range that gets edited, by default the marker 'leans_left' which means it goes to the beginning of the edited range.  If the field lean_right is set to true, the marker will lean right with edits and will go to the end of edited range.) */
     bool32 lean_right;
 };
 
