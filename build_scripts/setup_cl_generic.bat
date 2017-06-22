69fce6fcaffbd37f149c809a119394a8fb545e3f
Allen Webster <editor@4coder.net>
2017-06-22 19:28:19 -0400

    better vcvarsall.bat finder

--- build_scripts/setup_cl_generic.bat
+++ build_scripts/setup_cl_generic.bat
@@ -1,6 +1,18 @@
 @echo off
 
-IF NOT DEFINED LIB (call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %1)
-IF NOT DEFINED LIB (call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %1)
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 11.0
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 13.0
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
 
 
