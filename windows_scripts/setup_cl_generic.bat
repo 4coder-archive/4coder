dd2a86226391aa9c1d9bd86655ac89de50aa25c9
Allen Webster <editor@4coder.net>
2017-07-03 13:33:38 -0400

    adding VS 2015 to vcvarsall finder

--- windows_scripts/setup_cl_generic.bat
+++ windows_scripts/setup_cl_generic.bat
@@ -14,6 +14,9 @@ IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 13.0
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
 
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
 
