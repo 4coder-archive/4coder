c887a5ef735c5074b323e5208f921eb5db093e92
Allen Webster <editor@4coder.net>
2018-12-18 19:46:17 -0800

    Trying more stuff ...

--- meta/4ed_build.cpp
+++ meta/4ed_build.cpp
@@ -315,7 +315,7 @@ build(Partition *part, u32 flags, u32 arch, char *code_path, char **code_files, 
 "-Wno-unused-result"
 
 #define GCC_LIBS_COMMON                        \
-"-L/usr/lib -lX11 -lpthread -lm -lrt "   \
+"-L/usr/lib/i386-linux-gnu -lX11 -lpthread -lm -lrt "   \
 "-lGL -ldl -lXfixes -lfreetype -lfontconfig"
 
 #define GCC_LIBS_X64 GCC_LIBS_COMMON
