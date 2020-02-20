e91e76f3880b4f855e3cd2b23d6823698e248b73
Allen Webster <yoyo4thdimention@gmail.com>
2020-02-19 22:45:46 -0800

    2.1

--- platform_linux/linux_4ed.cpp
+++ platform_linux/linux_4ed.cpp
@@ -732,8 +732,8 @@ glx_create_context(GLXFBConfig fb_config){
         ctx = glXCreateNewContext(linuxvars.dpy, fb_config, GLX_RGBA_TYPE, 0, True);
     } else {
         static const int context_attribs[] = {
-            GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
-            GLX_CONTEXT_MINOR_VERSION_ARB, 2,
+            GLX_CONTEXT_MAJOR_VERSION_ARB, 2,
+            GLX_CONTEXT_MINOR_VERSION_ARB, 1,
             GLX_CONTEXT_PROFILE_MASK_ARB , GLX_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB,
 #if GL_DEBUG_MODE
             GLX_CONTEXT_FLAGS_ARB        , GLX_CONTEXT_DEBUG_BIT_ARB,
