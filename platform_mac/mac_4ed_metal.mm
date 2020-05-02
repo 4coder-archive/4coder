9b17ed2443ace68a97c0514de5f78d033778a3c5
Yuval Dolev <yuvaldolev2@gmail.com>
2020-05-03 01:04:32 +0300

    Fix for macOS input issue regarding dead keys.

--- platform_mac/mac_4ed_metal.mm
+++ platform_mac/mac_4ed_metal.mm
@@ -6,7 +6,7 @@
 
 struct Mac_Metal{
     Mac_Renderer base;
-    
+
     Metal_Renderer *renderer;
     MTKView *view;
 };
@@ -18,7 +18,7 @@ mac_render_sig(mac_metal__render){
 #if defined(FRED_INTERNAL)
     printf("Redering using Metal!\n");
 #endif
-    
+
     Mac_Metal *metal = (Mac_Metal*)renderer;
     [metal->view draw];
 }
@@ -28,7 +28,7 @@ mac_get_texture_sig(mac_metal__get_texture){
     Mac_Metal *metal = (Mac_Metal*)renderer;
     u32 result = [metal->renderer get_texture_of_dim:dim
             kind:texture_kind];
-    
+
     return(result);
 }
 
@@ -40,7 +40,7 @@ mac_fill_texture_sig(mac_metal__fill_texture){
             pos:p
             dim:dim
             data:data];
-    
+
     return(result);
 }
 
@@ -52,24 +52,24 @@ mac_metal__init(NSWindow *window, Render_Target *target){
     metal->base.render = mac_metal__render;
     metal->base.get_texture = mac_metal__get_texture;
     metal->base.fill_texture = mac_metal__fill_texture;
-    
+
     // NOTE(yuval): Create the Metal view
     NSView *content_view = [window contentView];
-    
+
     metal->view = [[MTKView alloc] initWithFrame:[content_view bounds]];
     [metal->view setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
     [metal->view setPaused:YES];
     [metal->view setEnableSetNeedsDisplay:NO];
-    
+
     metal->view.device = MTLCreateSystemDefaultDevice();
-    
+
     // NOTE(yuval): Add the Metal view as a subview of the window
     [content_view addSubview:metal->view];
-    
+
     // NOTE(yuval): Create the Metal renderer and set it as the Metal view's delegate
     metal->renderer = [[Metal_Renderer alloc] initWithMetalKitView:metal->view target:target];
     metal->view.delegate = metal->renderer;
-    
+
     return(metal);
 }
 
