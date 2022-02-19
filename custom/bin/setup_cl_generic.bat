b979e42f4f9d178e9067fe36f720d9af83fa7ae6
Allen Webster <yoyo4thdimention@gmail.com>
2022-02-19 09:51:02 -0800

    extend CL setup helper script to include 2022

--- custom/bin/setup_cl_generic.bat
+++ custom/bin/setup_cl_generic.bat
@@ -29,3 +29,9 @@ IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcv
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
 
+SET VC_PATH=C:\Program Files\Microsoft Visual Studio\2022\Community
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
+
+SET VC_PATH=C:\Program Files\Microsoft Visual Studio\2022\Professional
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
+
